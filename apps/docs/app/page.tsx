1cfce1b465e9950726c92ba39d4544c6faaf4e2b
shreeteja22 <smutukundu2206@gmail.com>
2026-03-14 20:55:31 +0530

    feat: a new application /docs for docs page

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Welcome to docs page! This is a simple app to test your mock APIs created with mockit. You can find the source code for this app on{" "}
+    </div>
+  )
+}
+
+export default page
